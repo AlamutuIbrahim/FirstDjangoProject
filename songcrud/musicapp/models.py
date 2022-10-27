de244c672a58313ba70185594cd9b896ddb0b02a
AlamutuIbrahim <abbeyabiodun06@gmail.com>
2022-10-27 22:31:38 +0100

    models registered at admin

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name
+
+class Song(models.Model):
+    artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=40)
+    date_released = models.DateField()
+    likes = models.IntegerField ()
+    artisteId = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Lyric(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE, null=True)
+    content= models.TextField()
+    songId = models.IntegerField()
+
+    def __str__(self):
+        return self.content
